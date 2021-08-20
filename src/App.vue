4a8e6aec7cc499bff723f1d37cdd66abf0f80a1c
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-08-20 20:54:57 -0300

    Background efects component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <background-effects></background-effects>
   </div>
 </template>
 
 <script>
+import BackgroundEffects from "@/components/BackgroundEffects.vue";
 
 export default {
   name: "App",
   components: {
+    BackgroundEffects,
   },
 };
 </script>
 
 <style lang="scss">
-
+background-effects {
+  position: absolute;
+}
 </style>
