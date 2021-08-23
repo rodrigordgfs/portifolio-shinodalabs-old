9818130aeb2b432ae46553125980ea27dad84259
Restyled.io <commits@restyled.io>
2021-08-23 22:24:20 +0000

    Restyled by clang-format

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
 import "./registerServiceWorker";
+
+import Vue from "vue";
+
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  render : (h) => h(App),
 }).$mount("#app");
