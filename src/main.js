42d64937e510028e29dd7287a07d0702d4609182
Restyled.io <commits@restyled.io>
2021-08-27 05:20:12 +0000

    Restyled by clang-format

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
 import "./registerServiceWorker";
-import VueSplide from "@splidejs/vue-splide";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+
+import VueSplide from "@splidejs/vue-splide";
+import Vue from "vue";
+
+import App from "./App.vue";
 
 Vue.use(VueSplide);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  render : (h) => h(App),
 }).$mount("#app");
