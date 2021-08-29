63dda11660db339d9396483e72e0e2c3b588ce47
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-08-29 16:08:02 -0300

    PWA Application

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import EasySlider from "vue-easy-slider";
+import "./registerServiceWorker";
 
 Vue.use(EasySlider);
 
